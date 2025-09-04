cb29fda5a8adb884923cfb2e21fe7f4ff240061e
Shankalpa Pokharel <pokharelshankalpa58@gmail.com>
2025-09-04 14:32:06 +0545

    dashboard setup

---
+++ app/admin/(protected)/page.tsx
@@ -0,0 +1,13 @@
+import { AppSidebar } from "@/components/admin-dashboard/app-sidebar"
+// import { DataTable } from "@/components/admin-dashboard/data-table"
+// import { SectionCards } from "@/components/admin-dashboard/section-cards"
+import { SiteHeader } from "@/components/admin-dashboard/site-header"
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+
+import data from "./data.json"
+
+export default function Page() {
+  return (
+   <div>Dashboard</div>
+  )
+}
